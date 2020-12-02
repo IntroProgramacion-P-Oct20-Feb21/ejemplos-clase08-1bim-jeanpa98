7d28545410f5cd86c084fca0e1b9dca41232e441
Jean Panamito <jeanpanamito@gmail.com>
2020-12-02 10:04:47 -0500

    cambios

--- ejemplos/EjemplosRepaso/src/ejemplosrepaso/Ejemplo02.java
+++ ejemplos/EjemplosRepaso/src/ejemplosrepaso/Ejemplo02.java
@@ -20,8 +20,19 @@ public class Ejemplo02 {
         // 
         Scanner entrada = new Scanner(System.in);
         String ciudad;
+       
+    
         String pais;
-        System.out.printf("%s%s", ciudad, pais);
+        
+                
+        System.out.println();
+        pais = entrada.nextLine();
+        System.out.println();
+        ciudad = entrada.nextLine();
+       
+        System.out.printf("%s,  situada en el sur del pais:\n\n\t%s\n", 
+                ciudad, pais);
+                
     }
     
 }
