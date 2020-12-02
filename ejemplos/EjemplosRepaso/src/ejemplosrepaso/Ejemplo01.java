bb8584e979a47522145e096b00532767391054a0
Jean Panamito <jeanpanamito@gmail.com>
2020-12-02 09:46:53 -0500

    cambios

--- ejemplos/EjemplosRepaso/src/ejemplosrepaso/Ejemplo01.java
+++ ejemplos/EjemplosRepaso/src/ejemplosrepaso/Ejemplo01.java
@@ -8,6 +8,8 @@
  */
 package ejemplosrepaso;
 
+import java.util.Scanner;
+
 
 public class Ejemplo01 {
 
@@ -19,7 +21,12 @@ public class Ejemplo01 {
         Scanner entrada = new Scanner(System.in);
         String ciudad;
         String pais;
-        System.out.printf("%s%s", ciudad, pais);
+        System.out.println("Ingrese el pais");
+        pais = entrada.nextLine();
+        System.out.println("Ingrese ciudad");
+        ciudad = entrada.nextLine();
+      
+        System.out.printf("%s, ciudad\n del pais:\n\t%s. \n", ciudad, pais);
     }
     
 }
